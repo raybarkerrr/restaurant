9b230e770a3add14a239b6e633d05ac95d4f3606
raybarkerrr <raybarkerrr@gmail.com>
2023-03-03 13:42:28 -0600

    Navbar implemented

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let mobileMenu = document.querySelector('.mobileMenu');
+let menuToggle = document.querySelector('.menuToggle');
+menuToggle.onclick = function() {
+   mobileMenu.classList.toggle('open');
+}
