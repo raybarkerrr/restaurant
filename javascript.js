47776d409d1abf6c2b1577d6f965b335b219e5f1
raybarkerrr <raybarkerrr@gmail.com>
2023-03-04 16:06:34 -0600

    Added API code

--- javascript.js
+++ javascript.js
@@ -3,3 +3,17 @@ let menuToggle = document.querySelector('.menuToggle');
 menuToggle.onclick = function() {
    mobileMenu.classList.toggle('open');
 }
+
+fetch ("https://free-food-menus-api-production.up.railway.app/steaks")
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    data.forEach(user => {
+        const markup = `<span>${user.name}</span>`;
+
+        document.querySelector(`span`) .insertAdjacentHTML("beforeend", markup )
+    });
+})
+
+.catch(error => console.log(error));
