2bf3179bcd0f0596bffbd9d4d0ba23da6b43cb2e
raybarkerrr <raybarkerrr@gmail.com>
2023-03-06 17:58:00 -0600

    Change to smoothie restaurant

--- javascript.js
+++ javascript.js
@@ -1,19 +1,8 @@
 let mobileMenu = document.querySelector('.mobileMenu');
 let menuToggle = document.querySelector('.menuToggle');
+
 menuToggle.onclick = function() {
    mobileMenu.classList.toggle('open');
 }
 
-fetch ("https://free-food-menus-api-production.up.railway.app/steaks")
-.then(response => {
-    return response.json();
-})
-.then(data => {
-    const firstRecord = data.shift();
-    console.log(firstRecord);
-    const markup = `<span>${data[2].name}</span>`;
-    document.querySelector(`span`).insertAdjacentHTML("beforeend", markup )
-})
-
-.catch(error => console.log(error));
 
