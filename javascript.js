7a7fe66a1ab4bb75a1710f3c1a2bc600874d34cf
raybarkerrr <raybarkerrr@gmail.com>
2023-03-07 15:55:21 -0600

    added api

--- javascript.js
+++ javascript.js
@@ -6,20 +6,45 @@ menuToggle.onclick = function() {
 }
 
 
-// Initialize and add the map
 function initMap() {
-   // The location of Uluru
-   const uluru = { lat: -25.344, lng: 131.031 };
-   // The map, centered at Uluru
+   
+   const yosemite = { lat: 37.88407138217377, lng: -119.51635674423457 };
+   
    const map = new google.maps.Map(document.getElementById("map"), {
      zoom: 4,
-     center: uluru,
+     center: yosemite,
    });
-   // The marker, positioned at Uluru
+   
    const marker = new google.maps.Marker({
-     position: uluru,
+     position: yosemite,
      map: map,
    });
  }
  
  window.initMap = initMap;
+
+
+const stripe = require('stripe')('sk_test_51MinZZKkGpyFNMs8UqRPdZ9H3TDMUJzxcf6ExJeBG0BicLZwzluiaA0yf5mep2zEyCjTcwTq0hPJxsZCSmAPoZYY004oxg2qiA');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'http://127.0.0.1:5500/';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: 'price_1MincuKkGpyFNMs8Is0Q4JIF',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
